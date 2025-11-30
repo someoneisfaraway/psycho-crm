4de5b4274358db533e7f9a1e34bcf41b6f73edc0
someone_there <someoneisfaraway@gmail.com>
2025-11-30 12:16:08 +0300

    fixed a bug where created sessions would disappear from the interface

--- src/pages/CalendarScreen.tsx
+++ src/pages/CalendarScreen.tsx
@@ -52,7 +52,9 @@ const CalendarScreen: React.FC = () => {
       try {
         const now = new Date();
         const startOfMonth = new Date(now.getFullYear(), now.getMonth(), 1);
+        startOfMonth.setHours(0, 0, 0, 0);
         const endOfMonth = new Date(now.getFullYear(), now.getMonth() + 1, 0);
+        endOfMonth.setHours(23, 59, 59, 999);
         const sessionsData = await sessionsApi.getForDateRange(user.id, startOfMonth, endOfMonth);
         setSessions(Array.isArray(sessionsData) ? (sessionsData as any) : []);
 
